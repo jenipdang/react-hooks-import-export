a2ffe6f87f2f721a7c4cad78e744283b6f1c1d16
Jeni Dang <jenipdang@yahoo.com>
2021-11-15 23:41:17 -0800

    commit tree 460f4829f9d12646112d9a8f434d8f5515153484

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,24 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerda from "./parks/MesaVerde";
+import { trees, wildlife } from "./parks/RockyMountain"
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
   return <h1>Colorado State Parks!</h1>;
 }
+
+function ColoradoStateParks() {
+  return (
+    <div>
+      <MesaVerda />
+    </div>
+  )
+}
+
+export default ColoradoStateParks;
+
+console.log(trees);
+
+wildlife();
